427c58a0a61571eca6d2c3dda8e5a22c5fc1b2b2
Сергей <dmitriy.ivanovich.1988@mail.ru>
2023-11-02 00:49:15 +0300

    исправил

---
+++ lr4 z1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static double f(double x)
+    {
+        return Math.Pow(x, 3) - Math.Sin(x);
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("введите a");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("введите b");
+        double b = double.Parse(Console.ReadLine());
+
+        double fa = f(a);
+        double fb = f(b);
+
+        if (fa > fb)
+        {
+            Console.WriteLine("Функция принимает наибольшее значение в точке a.");
+        }
+        else if (fa < fb)
+        {
+            Console.WriteLine("Функция принимает наибольшее значение в точке b.");
+        }
+        else
+        {
+            Console.WriteLine("Функция принимает одинаковые значения в точках a и b.");
+        }
+    }
+
+   
+}
